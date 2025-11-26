f58cc140fcae4b36f6b0ca815ca01f4bd51fa9a2
Sergio Torres <sergio.torres@idr.edu.mx>
2025-11-26 12:26:33 -0600

    feat: uso de bigint

--- js-01-data_types/src/js/main.js
+++ js-01-data_types/src/js/main.js
@@ -82,3 +82,20 @@ console.log( `MAX_SAFE_INTEGER + 2: ${Number.MAX_SAFE_INTEGER + 2 }`); // 900719
 console.log( `MAX_SAFE_INTEGER + 3: ${Number.MAX_SAFE_INTEGER + 3 }`); // 9007199254740994
 console.log( `MAX_SAFE_INTEGER + 4: ${Number.MAX_SAFE_INTEGER + 4 }`); // 9007199254740995
 console.log( `MAX_SAFE_INTEGER + 5: ${Number.MAX_SAFE_INTEGER + 5 }`); // 9007199254740996
+
+/*
+ Tipos de datos BigInt
+ Sirve para representar valores numéricos enteros, de los que el
+ tipo number no pueda representar o no es seguro.
+
+ Las operaciones con un bigInt se debe hacer con otro bigInt
+
+*/
+const myBigInt = 9007199254740991n;
+console.log( typeof myBigInt ); // bigint
+console.log( `Resultado de myBigInt + 1n: ${ myBigInt + 1n }`); // 9007199254740992
+console.log( `Resultado de myBigInt + 2n: ${ myBigInt + 2n }`); // 9007199254740993
+console.log( `Resultado de myBigInt + 3n: ${ myBigInt + 3n }`); // 9007199254740994
+console.log( `Resultado de myBigInt + 4n: ${ myBigInt + 4n }`); // 9007199254740995
+console.log( `Resultado de myBigInt + 5n: ${ myBigInt + 5n }`); // 9007199254740996
+console.log( `Resultado de myBigInt + 6n: ${ myBigInt + 6n }`); // 9007199254740997
